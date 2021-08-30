aa718aa9602bf5a986355fab9244640e3699764e
Erick Flores <ferick8246@gmail.com>
2021-08-29 21:06:04 -0500

    js code

---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+// Your age in days 
+
+function ageInDays(){
+var birthYear = prompt('What year were you born pimp?');
+var ageInDayss = (2021-birthYear) * 365;
+var h1 = document.createElement('h1');
+var textAnswer = document.createTextNode('You are'+ ageInDayss + 'days old');
+h1.setAttribute('id', 'ageInDays');
+h1.appendChild(textAnswer);
+document.getElementById('fle-box-result').appendChild(h1);
+
+}
