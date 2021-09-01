9c39acf2857349e8dd301384eb21043b0f1b0401
Erick Flores <ferick8246@gmail.com>
2021-08-31 22:32:34 -0500

    created the reset function

--- static/js/script.js
+++ static/js/script.js
@@ -8,5 +8,8 @@ var textAnswer = document.createTextNode('You are '+ ageInDayss + ' days old');
 h1.setAttribute('id', 'ageInDays');
 h1.appendChild(textAnswer);
 document.getElementById('flex-box-result').appendChild(h1);
+}
 
+function reset(){
+    document.getElementById('ageInDays').remove();
 }
