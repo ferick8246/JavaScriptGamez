df64107d11f0d050b1e2c9c01a7e7f54a2c07604
Erick Flores <ferick8246@gmail.com>
2021-09-01 23:50:01 -0500

    starting rock paper scissors game

--- static/js/script.js
+++ static/js/script.js
@@ -23,3 +23,31 @@ function generateCat() {
     div.appendChild(image);
 }
 
+// Rock Paper Game 
+function rpsGame(yourChoice){
+    console.log(yourChoice);
+    var humanChoice, botChoice;
+    //humanChoice = yourChoice.id;
+    botChoice=numberToChoice(randToRpsInt());
+    //results =decideWinner(humanChoice, botChoice);
+    //message= finalMessage(results); 
+    rpsFrontEnd(yourChoice.id, botChoice, message);
+}
+
+function randToRpsInt(){
+    return Math.floor(Math.random() * 3);
+}
+
+function numberToChoice(number) {
+    return ['rock', 'paper', 'scissors'][number]
+}
+
+function decideWinner(yourChoice, computerChoice){
+ var rpsDatabase = {
+     'rock': {'scissors':1, 'rock':0.5, 'paper':0},
+     'paper': {'rock':1, 'paper':0.5, 'scissors':0},
+     'scissors': {'paper':1, 'scissors':0.5, 'rock': 0},
+    }
+
+ var yourScore = rpsDatabase [yourChoice]
+}
