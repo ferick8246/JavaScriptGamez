00df1a02230863e436385866c4d748799c971464
Erick Flores <ferick8246@gmail.com>
2021-09-02 23:48:31 -0500

    added code for messege to decide winner

--- static/js/script.js
+++ static/js/script.js
@@ -24,35 +24,51 @@ function generateCat() {
 }
 
 // Rock Paper Game 
-function rpsGame(yourChoice){
+function rpsGame(yourChoice) {
     console.log(yourChoice);
     var humanChoice, botChoice;
     humanChoice = yourChoice.id;
+
     botChoice=numberToChoice(randToRpsInt());
     console.log('Computer choice:', botChoice);
+
     results =decideWinner(humanChoice, botChoice);
     console.log(results)
-    //message= finalMessage(results); 
+
+    message= finalMessage(results); 
+    console.log(message)
+
     //rpsFrontEnd(yourChoice.id, botChoice, message);
 }
 
-function randToRpsInt(){
+function randToRpsInt() {
     return Math.floor(Math.random() * 3);
 }
 
 function numberToChoice(number) {
-    return ['rock', 'paper', 'scissors'][number]
+    return ['rock', 'paper', 'scissors'][number];
 }
 
-function decideWinner(yourChoice, computerChoice){
+function decideWinner(yourChoice, computerChoice) {
  var rpsDatabase = {
-     'rock': {'scissors':1, 'rock':0.5, 'paper':0},
-     'paper': {'rock':1, 'paper':0.5, 'scissors':0},
-     'scissors': {'paper':1, 'scissors':0.5, 'rock': 0},
-    }
+     'rock': {'scissors': 1, 'rock': 0.5, 'paper': 0},
+     'paper': {'rock': 1, 'paper': 0.5, 'scissors': 0},
+     'scissors': {'paper': 1, 'scissors': 0.5, 'rock': 0}
+    };
 
- var yourScore = rpsDatabase [yourChoice][computerChoice]
- var computerScore = rpsDatabase[computerChoice][yourChoice]
- 
+ var yourScore = rpsDatabase [yourChoice][computerChoice];
+ var computerScore = rpsDatabase[computerChoice][yourChoice];
+
  return [yourScore, computerScore]
+}
+
+function finalMessage([yourScore, computerScore]){
+
+    if (yourScore === 0 ){
+        return {'message': 'You lost playa!', 'color': 'red'};
+    } else if (yourScore === 0.5) {
+        return {'message': 'You tied boss!', 'color': 'yellow'};
+    } else {
+        return {'message': 'You won pimp!', 'color': 'green'};
+    }
 }
