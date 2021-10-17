ac08df1ae695a943090e65c64fe3fe054676223f
Erick Flores <ferick8246@gmail.com>
2021-10-17 18:44:21 -0500

    Message code added

--- static/js/script.js
+++ static/js/script.js
@@ -83,9 +83,16 @@ function rpsFrontEnd(humanImageChoice, botImageChoice,finalMessage){
     document.getElementById('paper').remove();
     document.getElementById('scisors').remove();
 
+    var humanDiv = document.createElement('div');
+    var botDiv = document.createElement('div');
+    var messageDiv = document.createElement('div');
+
     humanDiv.innerHTML ="<img src='" + imagesDatabase[humanImageChoice] + "'height=150 width150 style='box-shadow: 0px 10px 50px rgba(37, 50, 233, 1);'>"
+    messageDiv.innerHTML = "h1 style='color: " + finalMessage['color'] + "; font-size: 60px; padding: 30px; '>" + finalMessage ['message'] + "</h1>"
     botDiv.innerHTML ="<img src='" + imagesDatabase[humanImageChoice] + "'height=150 width150 style='box-shadow: 0px 10px 50px rgba(243, 50, 24, 1);'>"
 
     document.getElementById ('flex-box-rps-div').appendChild(humanDiv);
+    document.getElementById ('flex-box-rps-div').appendChild(massageDiv);
     document.getElementById ('flex-box-rps-div').appendChild(botDiv);
+    
 }
