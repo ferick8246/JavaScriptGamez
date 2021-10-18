a9dd1b28696f2b3c3a9aa23eb4a0a180554ceec3
Erick Flores <ferick8246@gmail.com>
2021-10-17 19:23:29 -0500

    debbiging

--- static/js/script.js
+++ static/js/script.js
@@ -72,7 +72,7 @@ function finalMessage([yourScore, computerScore]){
         return {'message': 'You won pimp!', 'color': 'green'};
     }
 }
-function rpsFrontEnd(humanImageChoice, botImageChoice,finalMessage){
+function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage){
     var imageDatabase ={
         'rock': document.getElementById('rock').src,
         'paper': document.getElementById('paper').src,
@@ -87,9 +87,9 @@ function rpsFrontEnd(humanImageChoice, botImageChoice,finalMessage){
     var botDiv = document.createElement('div');
     var messageDiv = document.createElement('div');
 
-    humanDiv.innerHTML ="<img src='" + imageDatabase[humanImageChoice] + "'height=150 width150 style='box-shadow: 0px 10px 50px rgba(37, 50, 233, 1);'>"
+    humanDiv.innerHTML ="<img src='" + imageDatabase[humanImageChoice] + "'height=150 width=150 style='box-shadow: 0px 10px 50px rgba(37, 50, 233, 1);'>"
     messageDiv.innerHTML = "<h1 style='color: " + finalMessage['color'] + "; font-size: 60px; padding: 30px; '>" + finalMessage ['message'] + "</h1>"
-    botDiv.innerHTML ="<img src='" + imageDatabase[botImageChoice] + "'height=150 width150 style='box-shadow: 0px 10px 50px rgba(243, 50, 24, 1);'>"
+    botDiv.innerHTML ="<img src='" + imageDatabase[botImageChoice] + "'height=150 width=150 style='box-shadow: 0px 10px 50px rgba(243, 50, 24, 1);'>"
 
     document.getElementById ('flex-box-rps-div').appendChild(humanDiv);
     document.getElementById ('flex-box-rps-div').appendChild(massageDiv);
