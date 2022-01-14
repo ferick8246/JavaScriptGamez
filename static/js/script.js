fd5d286bcc32c8374951b666f50305ed6e2f746c
Erick Flores <ferick8246@gmail.com>
2022-01-13 20:44:26 -0600

    funtion for reset  button

--- static/js/script.js
+++ static/js/script.js
@@ -133,3 +133,9 @@ function buttonsGreen() {
     }
 }
 
+function buttonColorReset() {
+    for (let i=0;i < all_buttons.length; i++) {
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i] .classList.add(copyAllButtons[i]);
+    }
+}
